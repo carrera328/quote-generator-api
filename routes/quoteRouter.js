0fa390f8a5eb992f5d473d6fd0d51d4ed955402e
Sal Carrera <carrera.328@gmail.com>
2022-02-18 02:19:06 -0500

    hmmm

--- routes/quoteRouter.js
+++ routes/quoteRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 
-router.get('/', async (req, res) => {
+router.get('/casey', async (req, res) => {
 
     try{
         const result = await pool.query( `SELECT * FROM public."quote"`);
